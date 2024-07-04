5e87c9c5746f88fa2ade5df42834fa58e436ebb2
Amecani <amecani@student.42heilbronn.de>
2024-07-04 16:59:36 +0200

    Checks for : 1.The command clear && 2.If the given input is filled ONLY with spaces (' ')  or tabs (\t)

--- minishell.c
+++ minishell.c
@@ -6,18 +6,43 @@
 /*   By: amecani <amecani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 02:04:41 by amecani           #+#    #+#             */
-/*   Updated: 2024/07/03 03:21:31 by amecani          ###   ########.fr       */
+/*   Updated: 2024/07/04 15:25:45 by amecani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// add so if /// its red for wrong and ask potential teamate for new ones they might wanna add
+
+//! add this to parse_hub.c
+int	check_gaps_and_clear(char *s)
+{
+	int	i;
+
+	if (!ft_strncmp(s, "clear", 5) == 0 && \
+	(s[5] == '\0' || s[5] == ' ' || s[5] == '\t'))
+		return (free(s), printf("\033[H\033[J"), 0);
+	i = 0;
+	while (s[i++])
+		if (s[i] != ' ' || s[i] != '\t' || !s[i + 1])
+			return (0);
+	return (1);
+}
+
+int	command_center(t_terminal_inputs *terminal)
+{
+	terminal->input = readline("PROSI-shell$ :");
+	add_history(terminal->input);
+	if (!check_gaps_and_clear(terminal->input))
+		return (BAD_RECIEVE);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	if (argc != 1 && ft_strncp(argv[0], "./minishell", 11)) // add libft
-		return (prif("Type only : './minishell' \n")); // colour ./minishell with cyan
+	t_terminal_inputs	*terminal;
+
+	if (argc != 1) 										//! add libft
+		return (prif("Type only : './minishell' \n"));	//! colour ./minishell with cyan
 	while ("the program is running")
-		if (command_center() == BAD_RECIEVE)
+		if (command_center(terminal) == BAD_RECIEVE)
 			break ;
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: amecani <amecani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 03:04:51 by amecani           #+#    #+#             */
-/*   Updated: 2024/07/03 03:21:22 by amecani          ###   ########.fr       */
+/*   Updated: 2024/07/04 16:32:07 by amecani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 
 # define BAD_RECIEVE -7
 
+// Token Types ( learn enum )
+# define BAD_RECIEVE -7
+# define BAD_RECIEVE -7
+# define BAD_RECIEVE -7
+
 # include <readline/readline.h>
 
-typedef struct s_idk_yet
+typedef struct s_terminal_inputs
 {
-	char	*read_line;
-}	t_idk_yet;
+	char	*input;
+}	t_terminal_inputs;
 
 #endif
