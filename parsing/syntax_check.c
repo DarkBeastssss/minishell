162f4e12f4d6be7f9d4c786f06d9a9209cecb509
Amecani <amecani@student.42heilbronn.de>
2024-07-30 21:18:36 +0200

    Fixed one Syntax Error

--- parsing/syntax_check.c
+++ parsing/syntax_check.c
@@ -6,7 +6,7 @@
 /*   By: amecani <amecani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 23:34:54 by amecani           #+#    #+#             */
-/*   Updated: 2024/07/29 21:04:04 by amecani          ###   ########.fr       */
+/*   Updated: 2024/07/30 21:17:07 by amecani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,13 @@ int	syntax_check(t_token **token)
 	//what about them being the first one
 	while ((*token))
 	{
-		if ((*token)->type == PIPE && \
-		(!(*token)->prev || !(*token)->next || (*token)->prev->type != WORD || (*token)->next->type != WORD))
-			return (free_tokens(*token), printf("Syntax error\n"), 0);
-		if (((*token)->type == APPEND || (*token)->type == IN || (*token)->type == OUT || (*token)->type == H_DOC) \
-		 && (!(*token)->prev || (*token)->prev->type != WORD))
-			return (free_tokens(*token), printf("Syntax error\n"), 0);
-		(*token) = (*token)->next;
-
 		if ((*token)->next == NULL && \
 			(*token)->type != EMPTY && (*token)->type != WORD && (*token)->type != EMPTY)
 			return (printf("Syntax error near unexpected token `newline'\n"), free_tokens(first), 0); //! Fix Allocations free later
+		if (((*token)->type == PIPE || (*token)->type == APPEND || (*token)->type == IN || (*token)->type == OUT || (*token)->type == H_DOC) && \
+		((*token)->prev == NULL && (*token)->next == NULL && (*token)->prev->type != WORD && (*token)->next->type != WORD && (*token)->prev->type != EMPTY && (*token)->next->type != EMPTY))
+			return (free_tokens(*token), printf("Syntax error\n"), 0);
+		(*token) = (*token)->next;
 	}
 	// //*// testing poupeses
 	// get_first_token(token);
